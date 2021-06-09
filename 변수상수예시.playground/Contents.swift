830826531529d12630ced455be79237995344b12
김설희 <2750seolhee@naver.com>
2021-06-09 14:37:07 +0900

    상수 사용 이유 공부

---
+++ 변수상수예시.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// 변수, 상수 선언 예시
+var a = ""
+let b = ""
+
+// 상수를 사용하는 이유 예시
+func solution(_ n: Int) -> Bool {
+    let MaxNum = 100
+    return n < MaxNum
+}
+
+print(solution(0))
+
