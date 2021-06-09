df507d11146a862e3146613a341b687d12e72aee
김설희 <2750seolhee@naver.com>
2021-06-09 19:37:36 +0900

    Int 타입 범위 알아보기 예문 작성

---
+++ Int자료형_예문.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//print("Int8 : " + String(Int8.min) + " ~ " + String(Int8.max))
+//print("Int16 : " + String(Int16.min) + " ~ " + String(Int16.max))
+//print("Int32 : " + String(Int32.min) + " ~ " + String(Int32.max))
+//print("Int64 : " + String(Int64.min) + " ~ " + String(Int64.max))
+
+print("Int : " + String(Int.min) + " ~ " + String(Int.max))
+
+
+
+
