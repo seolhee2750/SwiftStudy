c2799fd4d824c7ad72143dcc989df14bbf04bba9
김설희 <2750seolhee@naver.com>
2021-06-12 02:56:15 +0900

    옵셔널 바인딩, 체이닝 연습

--- 옵셔널추출예문작성.playground/Contents.swift
+++ 옵셔널추출예문작성.playground/Contents.swift
@@ -12,13 +12,58 @@ import UIKit
 //
 //print(a, b)
 
-//옵셔널 바인딩을 이용한 옵셔널 추출
-var num: Int? = nil
+////옵셔널 바인딩을 이용한 옵셔널 추출
+//var num: Int? = nil
+//
+//if var number = num {
+//    number = 2
+//    print(number)
+//}
+//else {
+//    print("num is nil")
+//}
 
-if var number = num {
-    number = 2
-    print(number)
+class Person {
+    var name: String
+    var info: Info?
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Info {
+    var phoneNum: Int
+    var adress: Adress?
+    
+    init(phoneNum: Int) {
+        self.phoneNum = phoneNum
+    }
+}
+
+class Adress {
+    var city: String
+    var street: String?
+    
+    init(city: String) {
+        self.city = city
+    }
+}
+
+// 옵셔널 체이닝
+//let seolheeInfo: String? = seolhee.info?.adress?.street
+
+let seolhee: Person = Person(name: "seolhee")
+
+seolhee.info = Info(phoneNum: 0000)
+seolhee.info?.adress = Adress(city: "인천")
+seolhee.info?.adress?.street = "계양구"
+
+// 옵셔널 바인딩과 체이닝의 결합
+if let information: String = seolhee.info?.adress?.street {
+    print(information)
 }
 else {
-    print("num is nil")
+    print("Can not find")
 }
+
