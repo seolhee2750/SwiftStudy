d25792cbbd5c95d7aa0ffefd550e8fa87afeaeae
김설희 <2750seolhee@naver.com>
2021-06-12 00:41:30 +0900

    옵셔널 바인딩 연습

--- 옵셔널추출예문작성.playground/Contents.swift
+++ 옵셔널추출예문작성.playground/Contents.swift
@@ -1,7 +1,24 @@
 import UIKit
 
-var num1: Int? = 1
-var num2: Int = num1!
+//// 정상적인 강제 추출
+//var num1: Int? = 1
+//var num2: Int = num1!
+//
+//print(num1, num2)
 
-print(num1, num2)
-// Optional(1) 1
+//// 오류가 발생하는 강제 추출
+//var a: Int? = nil
+//var b: Int = a!
+//
+//print(a, b)
+
+//옵셔널 바인딩을 이용한 옵셔널 추출
+var num: Int? = nil
+
+if var number = num {
+    number = 2
+    print(number)
+}
+else {
+    print("num is nil")
+}
