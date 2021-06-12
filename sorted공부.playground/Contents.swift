e6d81c60911ee1203b7eef17ab4fd50da0a79934
김설희 <2750seolhee@naver.com>
2021-06-12 21:59:05 +0900

    sorted, joined의 활용 공부

--- sorted공부.playground/Contents.swift
+++ sorted공부.playground/Contents.swift
@@ -10,7 +10,15 @@ String(str.sorted())
 arr.joined()
 arr.sorted().joined()
 
-var arr2 = String(num).sorted()
-// arr2.joined()
-// 왜 안될까..ㅜㅜ하
+//var arr2 = String(num).sorted()
+//arr2.joined()
 
+var arr2 = String(num).sorted().map{String($0)}
+arr2.joined()
+
+// 위의 arr2의 타입을 보면 [String.Element]를 가지는 것을 볼 수 있음.
+// 하지만 joined는 [String] 타입을 받기 때문에 조인이 불가능 했던 것.
+// 그리고 sorted 자체도 [Character] 타입으로 반환하기 때문에
+// 저기에 바로 joined를 붙여주는 것도 불가능,,
+// 따라서 joined를 쓰고 싶으면 map으로 배열 요소들을 모두 스트링으로
+// 바꿔준 후에 사용해야 함.
