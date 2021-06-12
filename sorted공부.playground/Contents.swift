df9c09d69b3e655bcf26b884f1139450d733f8fb
김설희 <2750seolhee@naver.com>
2021-06-13 03:02:51 +0900

    블로그 업로드 용 sorted 예문 작성

--- sorted공부.playground/Contents.swift
+++ sorted공부.playground/Contents.swift
@@ -1,17 +1,20 @@
 import UIKit
 
+//var str = "54321"
+//var num = 54321
+//var arr = ["5", "4", "3", "2", "1"]
+
+//str.sorted()
+//String(str.sorted())
+//
+//arr.joined()
+//arr.sorted().joined()
+
 var str = "54321"
 var num = 54321
-var arr = ["5", "4", "3", "2", "1"]
 
-str.sorted()
-String(str.sorted())
-
-arr.joined()
-arr.sorted().joined()
-
-//var arr2 = String(num).sorted()
-//arr2.joined()
+//var arr = String(num).sorted()
+//arr.joined()
 
 var arr2 = String(num).sorted().map{String($0)}
 arr2.joined()
