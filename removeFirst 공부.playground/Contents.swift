c3bdbb81a208450f86a57b1468911fa6c4948ceb
김설희 <2750seolhee@naver.com>
2021-06-17 01:23:22 +0900

    프로그래머스 레벨2 다리를 지나는 트럭 풀던 도중,, 헷갈리는 부분들 체크!

---
+++ removeFirst 공부.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var arr = [1, 2, 3, 4]
+arr.removeFirst()
+arr
+arr[0]
+
+var arr2 = [10]
+arr2[0]
+arr2.removeFirst()
+arr2.count
+
+var a = 4
+
+while a < 5 {
+    a = 5
+    print(a)
+}
+
