05637c7b16265e58d07533e74be5dab6e19296bd
김설희 <2750seolhee@naver.com>
2021-06-22 19:04:09 +0900

    remove 함수 공부

--- 배열함수테스트.playground/Contents.swift
+++ 배열함수테스트.playground/Contents.swift
@@ -1,6 +1,11 @@
 import UIKit
 
-let arr = [1, 2, 2, 3, 3, 3]
+var arr = [1, 2, 2, 3, 3, 3]
 print(arr.filter({$0 == 3}).count)
 
 // filter 사용하면 배열에 있는 숫자 중복 횟수 확인 가능,,!!!!
+
+print(arr.remove(at: 1))
+print(arr)
+
+// remove는 먼저 해당 위치 요소를 반환하고 지움!
