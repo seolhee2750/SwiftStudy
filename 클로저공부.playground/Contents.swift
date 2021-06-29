19241adfe86df60b3ff3be972b72e97ca483edc8
김설희 <2750seolhee@naver.com>
2021-06-30 00:33:46 +0900

    클로저 공부 - 후행 클로저, 인라인 클로저

--- 클로저공부.playground/Contents.swift
+++ 클로저공부.playground/Contents.swift
@@ -3,6 +3,9 @@ import Foundation
 var arr = [1, 3, 2, 5, 4]
 
 arr.sorted(by: {(first: Int, second: Int) -> Bool in return first > second })
+arr.sorted() {(first: Int, second: Int) -> Bool in return first > second}
+
 arr.sorted(by: {$0 < $1})
 arr.sorted{$0 < $1}
+
 arr.sorted(by: <)
