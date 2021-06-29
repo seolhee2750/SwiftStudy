9503baab2856b68ab60d795c170fdb131f93d971
김설희 <2750seolhee@naver.com>
2021-06-30 01:42:36 +0900

    클로저 공부 - 연산자

--- 클로저공부.playground/Contents.swift
+++ 클로저공부.playground/Contents.swift
@@ -11,6 +11,15 @@ arr.sorted() {(first: Int, second: Int) -> Bool in return first > second}
 // 매개변수, 리턴 타입 생략
 arr.sorted() {(first, second) in return first > second}
 
+// 단축 인자 이름 사용
+arr.sorted{return $0 > $1}
+
+// 암시적 반환 표현
+arr.sorted{$0 > $1}
+
+// 연산자의 클로저 대체
+arr.sorted(by: >)
+
 arr.sorted(by: {$0 < $1})
 arr.sorted{$0 < $1}
 
