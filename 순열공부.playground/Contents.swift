ee909c98920186b688bc3a0ad1d1a68ec7b0fdc0
김설희 <2750seolhee@naver.com>
2021-06-30 21:15:56 +0900

    순열 정리 수정

--- 순열공부.playground/Contents.swift
+++ 순열공부.playground/Contents.swift
@@ -1,3 +1,8 @@
+// 참고
+//
+// https://turume.tistory.com/entry/Swift-%EC%89%BD%EA%B2%8C-%EC%9D%B4%ED%95%B4%ED%95%98%EB%8A%94-%EC%88%9C%EC%97%B4%EA%B3%BC-%EC%A1%B0%ED%95%A9
+//
+
 import Foundation
 
 func permuDFS(phase: Int) {
