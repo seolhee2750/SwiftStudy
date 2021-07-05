12969267ae68c72033f6ef510715397d363509b7
김설희 <2750seolhee@naver.com>
2021-07-05 22:52:11 +0900

    딕셔너리 타입

--- 타입공부.playground/Contents.swift
+++ 타입공부.playground/Contents.swift
@@ -21,6 +21,7 @@ print(s)
 print(arr[0])
 print(arr[1])
 
+// 딕셔너리는 없는 키 값도 부를 수 있고, 그 때는 nil 값을 반환. 따라서 옵셔널로 반환되는 것!!
 let dict = [ "Kim": 12, "Lee": 32 ]
 print(type(of: dict))
 print(dict["Lee"])
