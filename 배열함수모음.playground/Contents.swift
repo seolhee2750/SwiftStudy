9b483286bdcc78e505e46430a0e399501de11fba
김설희 <2750seolhee@naver.com>
2021-07-12 21:53:30 +0900

    sorted 정리

--- 배열함수모음.playground/Contents.swift
+++ 배열함수모음.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
+// 배열 관련 함수 정리
 
-var greeting = "Hello, playground"
+import Foundation
+
+var arr1 = [1, 2, 5, 4, 3]
+var arr2 = [1, 2, 5, 4, 3]
+var arr3 = [1, 2, 5, 4, 3]
+
+
+// [ 정렬 ]
+
+arr1.sorted() // 오름차순 정렬
+// 오름차순이 default라서 빈 괄호 가능
+
+arr1.sorted(by: <) // 오름차순 정렬
+arr1.sorted(by: >) // 내림차순 정렬
+
+arr2.sorted(){$0 > $1} // 오름차순 정렬
+arr2.sorted{$0 > $1} // 오름차순 정렬
+// 이렇게 클로저 사용한 경우에는 실제 배열에 반영x
+print(arr2)
+
