e0022d673f54d62882f96234877ba4b78dc543f2
김설희 <2750seolhee@naver.com>
2021-07-12 21:55:44 +0900

    수정

--- 배열함수모음.playground/Contents.swift
+++ 배열함수모음.playground/Contents.swift
@@ -2,12 +2,13 @@
 
 import Foundation
 
+//
+// [ 정렬 ]
+//
+
 var arr1 = [1, 2, 5, 4, 3]
 var arr2 = [1, 2, 5, 4, 3]
 var arr3 = [1, 2, 5, 4, 3]
-
-
-// [ 정렬 ]
 
 arr1.sorted() // 오름차순 정렬
 // 오름차순이 default라서 빈 괄호 가능
@@ -19,4 +20,3 @@ arr2.sorted(){$0 > $1} // 오름차순 정렬
 arr2.sorted{$0 > $1} // 오름차순 정렬
 // 이렇게 클로저 사용한 경우에는 실제 배열에 반영x
 print(arr2)
-
