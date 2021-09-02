348dd3016768278c5e073ad90c47a44938b7f516
김설희 <2750seolhee@naver.com>
2021-09-02 21:33:38 +0900

    split 파라미터 별 출력 비교 코멘트 추가

--- Summary/split 파라미터.playground/Contents.swift
+++ Summary/split 파라미터.playground/Contents.swift
@@ -1,12 +1,15 @@
+// split 파라미터 비교
+
+import Foundation
+
 var str = "apple grape cherry peach "
 
 // omittingEmptySubsequences는 빈 시퀀스를 포함하느냐 안하느냐의 유무를 결정.
 // true가 기본!
-print(str.split(separator: " ", omittingEmptySubsequences: false))
-print(str.split(separator: " ", omittingEmptySubsequences: true))
+print(str.split(separator: " ", omittingEmptySubsequences: false)) // ["apple", "grape", "cherry", "peach", ""]
+print(str.split(separator: " ", omittingEmptySubsequences: true)) // ["apple", "grape", "cherry", "peach"]
 
 
 // maxSplit은 Int 입력받고, 0보다 크거나 같아야하며 Int.max가 기본값.
-//print(str.split(separator: " ", maxSplits: 1))
-//print(str.split(separator: " ", maxSplits: 2))
-
+print(str.split(separator: " ", maxSplits: 1)) // ["apple", "grape cherry peach "]
+print(str.split(separator: " ", maxSplits: 2)) // ["apple", "grape", "cherry peach "]
