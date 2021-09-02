3f83a10450366ab40ed03a782f7a113ed72e0f0b
김설희 <2750seolhee@naver.com>
2021-09-03 01:29:02 +0900

    switch 문에서 튜플을 사용하는 방법

---
+++ 데이터 타입/튜플/switch에서의 튜플 사용.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let coordinate = (x: 1, y: 2)
+
+switch coordinate {
+case (0, 0):
+    print("원점입니다.")
+case (_, 0): // 구분에 필요없는 요소는 _로 표현 가능
+    print("x축입니다.")
+case (0, let a): // 원하는 요소 값을 입력받아 사용 가능
+    print("y축이며, y좌표는 \(a)입니다.")
+default:
+    print("(\(coordinate.x), \(coordinate.y))입니다.")
+}
+
+// (1, 2)입니다.
