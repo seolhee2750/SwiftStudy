01e5b5dd7a928b5654c6980d91b96285921cc508
김설희 <2750seolhee@naver.com>
2021-09-03 02:04:50 +0900

    튜플 switch 사용시 where절 사용 방법 추가

--- 데이터 타입/튜플/switch에서의 튜플 사용.playground/Contents.swift
+++ 데이터 타입/튜플/switch에서의 튜플 사용.playground/Contents.swift
@@ -9,8 +9,10 @@ case (_, 0): // 구분에 필요없는 요소는 _로 표현 가능
     print("x축입니다.")
 case (0, let a): // 원하는 요소 값을 입력받아 사용 가능
     print("y축이며, y좌표는 \(a)입니다.")
+case (let i, let j) where i == j: // where절 사용 가능
+        print("x, y축 좌표가 동일합니다.")
 default:
     print("(\(coordinate.x), \(coordinate.y))입니다.")
 }
 
-// (1, 2)입니다. .
+// (1, 2)입니다.
