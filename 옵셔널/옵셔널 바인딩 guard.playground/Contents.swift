2507ac2a2842976f612de0d248acb053e1a5d9af
김설희 <2750seolhee@naver.com>
2021-09-06 23:54:11 +0900

    if let 구문 수정

--- 옵셔널/옵셔널 바인딩 guard.playground/Contents.swift
+++ 옵셔널/옵셔널 바인딩 guard.playground/Contents.swift
@@ -4,11 +4,17 @@ let value_1: Int? = nil
 let value_2: Int? = 2
 
 // if let 구문을 활용한 옵셔널 바인딩
-if let tmp = value_1 { print(tmp, "입니다.") }
-else { print("nil 입니다.") }
+func PrintValue_if(_ v: Int?) {
+    if let tmp = v {
+        print(tmp, "입니다.")
+    }
+    else {
+        print("nil 입니다.")
+    }
+}
 
 // guard let 구문을 활용한 옵셔널 바인딩
-func PrintValue(_ v: Int?) {
+func PrintValue_guard(_ v: Int?) {
     guard let tmp_g = v else {
         print("nil 입니다.")
         return
@@ -17,9 +23,12 @@ func PrintValue(_ v: Int?) {
     print(tmp_g, "입니다.")
 }
 
-PrintValue(value_1)
-PrintValue(value_2)
-
+PrintValue_if(value_1)
+PrintValue_if(value_2)
 // nil 입니다.
+// 2 입니다.
+
+PrintValue_guard(value_1)
+PrintValue_guard(value_2)
 // nil 입니다.
 // 2 입니다.
