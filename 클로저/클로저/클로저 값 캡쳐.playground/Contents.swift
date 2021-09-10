af426284b3e715b838f8d5fa2eee9561568eae09
김설희 <2750seolhee@naver.com>
2021-09-10 17:24:01 +0900

    값 캡쳐 예시 작성

--- 클로저/클로저/클로저 값 캡쳐.playground/Contents.swift
+++ 클로저/클로저/클로저 값 캡쳐.playground/Contents.swift
@@ -1,3 +1,38 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// [ 값 캡쳐 기본 형태 ]
+
+var count = 0
+
+let incrementer = {
+    count += 1
+}
+
+incrementer()
+incrementer()
+incrementer()
+
+count // 3
+
+
+// [ 중첩 함수로 보는 값 캡쳐 ]
+
+func add(num: Int) -> (() -> Int) {
+    var total = 0
+    func adding() -> Int {
+        total += num
+        return total
+    }
+    return adding
+}
+
+let add_1 = add(num: 1)
+let add_2 = add(num: 1)
+
+add_1() // 1
+add_1() // 2
+add_1() // 3
+
+add_2() // 1
+add_2() // 2
+add_2() // 3
