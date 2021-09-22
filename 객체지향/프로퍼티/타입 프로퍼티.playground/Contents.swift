a85b915ef2acf055f2bbf6e4ff8b936d694588c1
김설희 <2750seolhee@naver.com>
2021-09-22 14:47:26 +0900

    연산 타입 프로퍼티 사용 예시 및 설명

--- 객체지향/프로퍼티/타입 프로퍼티.playground/Contents.swift
+++ 객체지향/프로퍼티/타입 프로퍼티.playground/Contents.swift
@@ -14,8 +14,22 @@ class seolhee_Info {
     static var age: Int = 1
 }
 
-seolhee_Info.age = 2
+// 인스턴스에 접근할 필요 없이 타입 이름만으로 프로퍼티 사용 가능
+seolhee_Info.age = 2 // var로 선언된 경우 값 변경도 가능
 print(seolhee_Info.name) // 설희
 print(seolhee_Info.age) // 2
 
 
+// ----------------------------------------
+// (2) 연산 타입 프로퍼티
+// => 연산 프로퍼티 앞에 static 키워드를 붙인 것.
+// 변수로만 선언 가능
+
+class seolhee_Info2 {
+    static var introduce: String {
+        return "설희"
+    }
+}
+
+print(seolhee_Info2.introduce) // 설희
+seolhee_Info2.introduce
