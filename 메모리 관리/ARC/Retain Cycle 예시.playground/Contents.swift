a24269bbb7771ec3ce6e18c95e1a4881c638ec47
김설희 <2750seolhee@naver.com>
2021-10-02 22:11:22 +0900

    Retain Cycle 문제의 수동 해결 방법

--- 메모리 관리/ARC/Retain Cycle 예시.playground/Contents.swift
+++ 메모리 관리/ARC/Retain Cycle 예시.playground/Contents.swift
@@ -28,3 +28,11 @@ test2?.B = test1 // Example_A 참조 횟수 : 2
 
 test1 = nil // Example_A 참조 횟수 : 1
 test2 = nil // Example_B 참조 횟수 : 1
+
+/* 강한참조 순환 문제 수동 해결 방법,,!!
+test1?.A = nil
+test1 = nil
+
+test2?.B = nil
+test2 = nil
+*/
